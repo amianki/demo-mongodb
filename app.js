d846daba6792ec0873aaf286c2ba6ae7e10ff1a6
amianki <ankita.mandal201@gmail.com>
2023-06-09 01:09:00 +0530

    demoMongodb connected

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const url = "mongodb://localhost:27017"
 const client = new mongodb(url);
 const localDB = client.db("local");
 const demoCollection = localDB.collection("demoCollection");
-console.log(demoCollection.find({}).toArray()); 
+demoCollection.find({}).toArray().then((data)=>{
+    console.log(data);
+})
