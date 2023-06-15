40642c0c74348b5c300d8fdf3dfaac4184277c05
amianki <ankita.mandal201@gmail.com>
2023-06-15 20:00:19 +0530

    updated

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const url = "mongodb://localhost:27017"
 const client = new mongodb(url);
 const localDB = client.db("local");
 const demoCollection = localDB.collection("demoCollection");
-demoCollection.find({}).toArray().then((data)=>{
-    console.log(data);
-})
+// For READ data...
+// demoCollection.find({}).toArray().then((data)=>{
+//     console.log(data);
+// })
+//for CREATE data
+demoCollection.insertOne({"name":"Babli"}).then(console.log)
+
+
+
+
